aead25c37ecedd31fb2fd34401ee9bbe52f4b75d
Andrey Isenko <aisenko87@gmail.com>
2025-09-19 23:44:38 +0300

    add-tren

---
+++ js-module-2-1.js
@@ -0,0 +1,48 @@
+// let sum = 0;
+// function foo(params) {
+//   console.log(params);
+//   console.log(params[params.length - 1]);
+
+//   sum += params.length;
+//   for (const element of params) {
+//     // console.log(element);
+//     sum += element;
+//   }
+//   return sum;
+// }
+
+// // foo([1, 2, 3, 4, 5]);
+// console.log(foo([1, 2, 3, 4, 5]));
+
+// const arr = ['HTML', 'JS', 'CSS', 'React'];
+// const arr2 = arr.slice();
+// console.log(arr2);
+
+// const newArr = arr.splice(1, 2, 'Node', 'Postgres');
+// // console.log(arr);
+// // console.log(newArr);
+// // console.log(arr);
+// console.log(arr2);
+// const rew = arr2.reverse();
+// console.log(rew);
+// console.log(rew[rew.length - 1]);
+
+// const arr = ['HTML', 'JS', 'CSS', 'React'];
+// const arr2 = arr.slice(0, 2);
+// console.log(arr2);
+// console.log(arr);
+
+const playList = {
+  name: 'Bob',
+  rating: 4,
+  isFavorite: true,
+  tracks: ['tracks-1', 'tracks-2', 'tracks-3'],
+};
+playList.name = 'Didi';
+playList.fulMane = 'Bob Marly';
+// console.log(playList);
+// console.log(playList.fulMane);
+// console.log(playList.fulMane);
+// console.log(playList.tracks[1]);
+console.log(playList);
+console.dir(playList);
