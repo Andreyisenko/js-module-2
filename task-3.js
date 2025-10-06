2e8b3df69f9dbd117e611bfb6f56ab3c89b43ee0
Andrey Isenko <aisenko87@gmail.com>
2025-10-06 21:36:30 +0300

    add-task-5

---
+++ task-3.js
@@ -0,0 +1,68 @@
+// console.log('task-3');
+
+// const users = [
+//   { name: 'Alice', age: 25, isActive: true, balance: 1200 },
+//   { name: 'Bob', age: 30, isActive: false, balance: 800 },
+//   { name: 'Charlie', age: 22, isActive: true, balance: 1500 },
+//   { name: 'Diana', age: 28, isActive: true, balance: 600 },
+//   { name: 'Eve', age: 35, isActive: false, balance: 2200 },
+// ];
+
+// const userName = users.map(user => {
+//   return user.name;
+// });
+// console.log(userName);
+
+// const userIsOnline = users.filter(user => user.isActive === true);
+// console.log(userIsOnline);
+// const num = JSON.stringify([1]);
+// console.log(num);
+// console.log(typeof num);
+// const arrNum = JSON.parse(num);
+// console.log(arrNum);
+// console.log(typeof arrNum);
+// console.log(JSON.stringify([1]) === JSON.stringify([Number('1')]));
+// console.log(JSON.stringify([1]) === JSON.stringify([+'1']));
+
+// console.log([].length === [].length);
+// console.log([1, 3, 4, 'wwq', 6][3].length);
+
+// // console.log(numbers);
+// const numbers = [5, 12, 8, 20, 3, 15, 9, 1, 27, 18];
+// // const arr = numbers.map((el, index, arraY) => el);
+// // console.log(arr);
+
+// // const newArr = numbers.filter((el, index) => el > 10);
+// // console.log(newArr);
+
+// const sum = numbers.reduce((acc, el, i, arrr) => acc + el, 0);
+// const num = sum / numbers.length;
+// console.log(sum.toFixed(1));
+// console.log(num.toFixed(1));
+// // console.log(sum.toFixed(3));
+// // console.log(Math.floor(sum));
+// // console.log(Math.ceil(sum));
+// // console.log(Math.round(sum));
+// // console.log(sum.toFixed(1));
+// console.log([][1]);
+
+const arr = new Array(15);
+console.log(arr);
+// console.log(arr.length); //
+// const arre = [
+//   undefined,
+//   undefined,
+//   undefined,
+//   undefined,
+//   undefined,
+//   undefined,
+//   undefined,
+//   undefined,
+//   undefined,
+// ];
+// undefined, console.log(arre);
+// const res = arre.map(el => {
+//   console.log(el);
+//   return el;
+// });
+// console.log(res);
