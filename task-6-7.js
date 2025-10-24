7d0c6c3be8946d047a9bf4979be38feab15ebe72
Andrey Isenko <aisenko87@gmail.com>
2025-10-24 18:25:39 +0300

    task-6-7

---
+++ task-6-7.js
@@ -0,0 +1,19 @@
+// const tot = document.querySelectorAll('.scri');
+// console.log(Array.isArray(tot));
+// const arrTot = Array.from(tot);
+// console.log(Array.isArray(arrTot));
+// console.log(tot.length);
+// console.log(arrTot.map(elem => elem));
+
+// console.log(tot.src);
+// console.log(!isNaN('12'));
+// const num = '223.58943e38437';
+// console.log(num);
+// // const isNum = parseInt(num);
+// const isNum = parseFloat(num);
+// console.log(isNum);
+
+// console.log(isNum.toFixed(1));
+const link = document.querySelector('.link');
+console.log(link.href);
+console.log(link.getAttribute('href'));
